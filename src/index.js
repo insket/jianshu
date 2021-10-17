6f89642701bd2b6d60cf4bdcb5f428699fcb81d1
insket <2694183181@qq.com>
2021-10-17 22:31:31 +0800

    'init'

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { Globalstyle } from './style.js';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Fragment>
+    <Globalstyle/>
     <App />
-  </React.StrictMode>,
+  </Fragment>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
